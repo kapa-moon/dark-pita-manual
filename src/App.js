90817e1fb14931aed20aebf9c7e6bda149e992af
Yuewen <yuewenkapa@gmail.com>
2022-08-17 10:10:37 -0400

    pita face update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
 
-      <img className="PitaAvatar" src="https://cdn.glitch.global/437de514-4247-434b-b3ad-750c6fc27691/DALL%C2%B7E%202022-08-04%2012.53.10%20-%20pita%20bread%20in%20cartoon%20style%203D%20rendered.png?v=1660717926745" />
+      <img className="PitaAvatar" src="https://cdn.glitch.global/437de514-4247-434b-b3ad-750c6fc27691/pita_face.png?v=1660745374412" />
       <h1>Welcome to Dark Pita :)</h1>
       <Step title="Step 0 Installation" image="https://cdn.glitch.global/437de514-4247-434b-b3ad-750c6fc27691/ytb_side_blkpreview.gif?v=1660685057356" content=""></Step>
       <StepOne></StepOne>
